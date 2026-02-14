6ae663c8d04faec48de7f790d3e08d78018f4a42
jneema <joeyshem33@gmail.com>
2026-02-14 15:50:13 +0300

    small bug fix

--- app/collections/[id].jsx
+++ app/collections/[id].jsx
@@ -256,6 +256,7 @@ export default function CollectionDetail() {
                             params: {
                               collectionId: id,
                               collectionName: collection?.name,
+                              query: collection?.name
                             },
                           });
                         }}
--- app/search.jsx
+++ app/search.jsx
@@ -33,6 +33,7 @@ export default function SearchScreen() {
   const router = useRouter();
   const insets = useSafeAreaInsets();
   const params = useLocalSearchParams();
+  console.log(params)
   const colorScheme = useColorScheme();
   const initialSearchDone = useRef(false);
 
