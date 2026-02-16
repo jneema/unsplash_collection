c3559c7e4fa38eae72174eafad1bcdc579529171
jneema <joeyshem33@gmail.com>
2026-02-16 17:57:13 +0300

    Hardcoding URL to fix scheme error

--- api/index.js
+++ api/index.js
@@ -1,25 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true",
-  },
+  baseURL: "https://dutch-anjela-my-org-org-f52e9afe.koyeb.app",
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     console.log("NETWORK ERROR DETAIL:", {
       message: error.message,
       code: error.code,
       config: error.config.url,
-      response: error.response?.data
+      response: error.response?.data,
     });
     return Promise.reject(error);
-  }
+  },
 );
-
 
 export default api;
